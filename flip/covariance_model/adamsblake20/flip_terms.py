d57f3d376400ee747f82e98b87e6425a21e45000
corentinravoux <corentin.ravoux.research@gmail.com>
2023-08-22 12:25:33 +0200

    fix of flip terms

--- flip/covariance_model/adamsblake20/flip_terms.py
+++ flip/covariance_model/adamsblake20/flip_terms.py
@@ -1,35 +1,34 @@
 import numpy as np
 
 
-def K_gg_0_0(theta, phi, k, sig_g):
+def K_gg_0_0(theta, phi, sig_g):
     def func(k):
         return (1 / 2) * np.sqrt(np.pi) * np.erf(k * sig_g) / (k * sig_g)
 
     return func
 
 
-def K_gg_0_1(theta, phi, k, sig_g):
+def K_gg_0_1(theta, phi, sig_g):
     def func(k):
         return 0
 
     return func
 
 
-def K_gg_0_2(theta, phi, k, sig_g):
+def K_gg_0_2(theta, phi, sig_g):
     def func(k):
         return (
-            -5
-            / 32
-            * (3 * np.cos(2 * phi) + 1)
+            (5 / 16)
+            * (3 * np.cos(phi) ** 2 - 1)
             * (
-                2
+                -2
                 * np.sqrt(np.pi)
                 * k**2
                 * sig_g**2
                 * np.exp(k**2 * sig_g**2)
                 * np.erf(k * sig_g)
-                + 6 * k * sig_g
-                - 3 * np.sqrt(np.pi) * np.exp(k**2 * sig_g**2) * np.erf(k * sig_g)
+                - 6 * k * sig_g
+                + 3 * np.sqrt(np.pi) * np.exp(k**2 * sig_g**2) * np.erf(k * sig_g)
             )
             * np.exp(-(k**2) * sig_g**2)
             / (k**3 * sig_g**3)
@@ -38,34 +37,42 @@ def K_gg_0_2(theta, phi, k, sig_g):
     return func
 
 
-def K_gg_0_3(theta, phi, k, sig_g):
+def K_gg_0_3(theta, phi, sig_g):
     def func(k):
         return 0
 
     return func
 
 
-def K_gg_0_4(theta, phi, k, sig_g):
+def K_gg_0_4(theta, phi, sig_g):
     def func(k):
         return (
-            (9 / 4096)
-            * (20 * np.cos(2 * phi) + 35 * np.cos(4 * phi) + 9)
+            (9 / 512)
             * (
                 12
                 * np.sqrt(np.pi)
                 * k**4
                 * sig_g**4
+                * (35 * np.cos(phi) ** 4 - 30 * np.cos(phi) ** 2 + 3)
                 * np.exp(k**2 * sig_g**2)
                 * np.erf(k * sig_g)
-                - 20 * k**3 * sig_g**3
+                + 20
+                * k**3
+                * sig_g**3
+                * (-35 * np.cos(phi) ** 4 + 30 * np.cos(phi) ** 2 - 3)
                 - 60
                 * np.sqrt(np.pi)
                 * k**2
                 * sig_g**2
+                * (35 * np.cos(phi) ** 4 - 30 * np.cos(phi) ** 2 + 3)
                 * np.exp(k**2 * sig_g**2)
                 * np.erf(k * sig_g)
-                - 210 * k * sig_g
-                + 105 * np.sqrt(np.pi) * np.exp(k**2 * sig_g**2) * np.erf(k * sig_g)
+                - 210 * k * sig_g * (35 * np.cos(phi) ** 4 - 30 * np.cos(phi) ** 2 + 3)
+                + 105
+                * np.sqrt(np.pi)
+                * (35 * np.cos(phi) ** 4 - 30 * np.cos(phi) ** 2 + 3)
+                * np.exp(k**2 * sig_g**2)
+                * np.erf(k * sig_g)
             )
             * np.exp(-(k**2) * sig_g**2)
             / (k**5 * sig_g**5)
@@ -74,45 +81,41 @@ def K_gg_0_4(theta, phi, k, sig_g):
     return func
 
 
-def K_gg_1_0(theta, phi, k, sig_g):
+def K_gg_1_0(theta, phi, sig_g):
     def func(k):
-        return (
-            -1
-            / 4
-            * (
-                2 * k * sig_g
-                - np.sqrt(np.pi) * np.exp(k**2 * sig_g**2) * np.erf(k * sig_g)
-            )
-            * np.exp(-(k**2) * sig_g**2)
-            / (k**3 * sig_g**3)
-        )
+        return -1 / 2 * np.exp(-(k**2) * sig_g**2) / (k**2 * sig_g**2) + (
+            1 / 4
+        ) * np.sqrt(np.pi) * np.erf(k * sig_g) / (k**3 * sig_g**3)
 
     return func
 
 
-def K_gg_1_1(theta, phi, k, sig_g):
+def K_gg_1_1(theta, phi, sig_g):
     def func(k):
         return 0
 
     return func
 
 
-def K_gg_1_2(theta, phi, k, sig_g):
+def K_gg_1_2(theta, phi, sig_g):
     def func(k):
         return (
-            -5
-            / 64
-            * (3 * np.cos(2 * phi) + 1)
+            (5 / 32)
             * (
-                8 * k**3 * sig_g**3
-                + 2
+                8 * k**3 * sig_g**3 * (1 - 3 * np.cos(phi) ** 2)
+                - 2
                 * np.sqrt(np.pi)
                 * k**2
                 * sig_g**2
+                * (3 * np.cos(phi) ** 2 - 1)
                 * np.exp(k**2 * sig_g**2)
                 * np.erf(k * sig_g)
-                + 18 * k * sig_g
-                - 9 * np.sqrt(np.pi) * np.exp(k**2 * sig_g**2) * np.erf(k * sig_g)
+                - 18 * k * sig_g * (3 * np.cos(phi) ** 2 - 1)
+                + 9
+                * np.sqrt(np.pi)
+                * (3 * np.cos(phi) ** 2 - 1)
+                * np.exp(k**2 * sig_g**2)
+                * np.erf(k * sig_g)
             )
             * np.exp(-(k**2) * sig_g**2)
             / (k**5 * sig_g**5)
@@ -121,36 +124,46 @@ def K_gg_1_2(theta, phi, k, sig_g):
     return func
 
 
-def K_gg_1_3(theta, phi, k, sig_g):
+def K_gg_1_3(theta, phi, sig_g):
     def func(k):
         return 0
 
     return func
 
 
-def K_gg_1_4(theta, phi, k, sig_g):
+def K_gg_1_4(theta, phi, sig_g):
     def func(k):
         return (
-            -9
-            / 8192
-            * (20 * np.cos(2 * phi) + 35 * np.cos(4 * phi) + 9)
+            (9 / 1024)
             * (
-                64 * k**5 * sig_g**5
-                - 12
+                64
+                * k**5
+                * sig_g**5
+                * (-35 * np.cos(phi) ** 4 + 30 * np.cos(phi) ** 2 - 3)
+                + 12
                 * np.sqrt(np.pi)
                 * k**4
                 * sig_g**4
+                * (35 * np.cos(phi) ** 4 - 30 * np.cos(phi) ** 2 + 3)
                 * np.exp(k**2 * sig_g**2)
                 * np.erf(k * sig_g)
-                + 340 * k**3 * sig_g**3
-                + 180
+                + 340
+                * k**3
+                * sig_g**3
+                * (-35 * np.cos(phi) ** 4 + 30 * np.cos(phi) ** 2 - 3)
+                - 180
                 * np.sqrt(np.pi)
                 * k**2
                 * sig_g**2
+                * (35 * np.cos(phi) ** 4 - 30 * np.cos(phi) ** 2 + 3)
                 * np.exp(k**2 * sig_g**2)
                 * np.erf(k * sig_g)
-                + 1050 * k * sig_g
-                - 525 * np.sqrt(np.pi) * np.exp(k**2 * sig_g**2) * np.erf(k * sig_g)
+                - 1050 * k * sig_g * (35 * np.cos(phi) ** 4 - 30 * np.cos(phi) ** 2 + 3)
+                + 525
+                * np.sqrt(np.pi)
+                * (35 * np.cos(phi) ** 4 - 30 * np.cos(phi) ** 2 + 3)
+                * np.exp(k**2 * sig_g**2)
+                * np.erf(k * sig_g)
             )
             * np.exp(-(k**2) * sig_g**2)
             / (k**7 * sig_g**7)
@@ -159,15 +172,14 @@ def K_gg_1_4(theta, phi, k, sig_g):
     return func
 
 
-def K_gg_2_0(theta, phi, k, sig_g):
+def K_gg_2_0(theta, phi, sig_g):
     def func(k):
         return (
-            -1
-            / 8
+            (1 / 8)
             * (
-                4 * k**3 * sig_g**3
-                + 6 * k * sig_g
-                - 3 * np.sqrt(np.pi) * np.exp(k**2 * sig_g**2) * np.erf(k * sig_g)
+                -4 * k**3 * sig_g**3
+                - 6 * k * sig_g
+                + 3 * np.sqrt(np.pi) * np.exp(k**2 * sig_g**2) * np.erf(k * sig_g)
             )
             * np.exp(-(k**2) * sig_g**2)
             / (k**5 * sig_g**5)
@@ -176,30 +188,33 @@ def K_gg_2_0(theta, phi, k, sig_g):
     return func
 
 
-def K_gg_2_1(theta, phi, k, sig_g):
+def K_gg_2_1(theta, phi, sig_g):
     def func(k):
         return 0
 
     return func
 
 
-def K_gg_2_2(theta, phi, k, sig_g):
+def K_gg_2_2(theta, phi, sig_g):
     def func(k):
         return (
-            -5
-            / 128
-            * (3 * np.cos(2 * phi) + 1)
+            (5 / 64)
             * (
-                16 * k**5 * sig_g**5
-                + 48 * k**3 * sig_g**3
-                + 6
+                16 * k**5 * sig_g**5 * (1 - 3 * np.cos(phi) ** 2)
+                + 48 * k**3 * sig_g**3 * (1 - 3 * np.cos(phi) ** 2)
+                - 6
                 * np.sqrt(np.pi)
                 * k**2
                 * sig_g**2
+                * (3 * np.cos(phi) ** 2 - 1)
                 * np.exp(k**2 * sig_g**2)
                 * np.erf(k * sig_g)
-                + 90 * k * sig_g
-                - 45 * np.sqrt(np.pi) * np.exp(k**2 * sig_g**2) * np.erf(k * sig_g)
+                - 90 * k * sig_g * (3 * np.cos(phi) ** 2 - 1)
+                + 45
+                * np.sqrt(np.pi)
+                * (3 * np.cos(phi) ** 2 - 1)
+                * np.exp(k**2 * sig_g**2)
+                * np.erf(k * sig_g)
             )
             * np.exp(-(k**2) * sig_g**2)
             / (k**7 * sig_g**7)
@@ -208,38 +223,48 @@ def K_gg_2_2(theta, phi, k, sig_g):
     return func
 
 
-def K_gg_2_3(theta, phi, k, sig_g):
+def K_gg_2_3(theta, phi, sig_g):
     def func(k):
         return 0
 
     return func
 
 
-def K_gg_2_4(theta, phi, k, sig_g):
+def K_gg_2_4(theta, phi, sig_g):
     def func(k):
         return (
-            -9
-            / 16384
-            * (20 * np.cos(2 * phi) + 35 * np.cos(4 * phi) + 9)
+            (9 / 2048)
             * (
-                128 * k**7 * sig_g**7
-                + 832 * k**5 * sig_g**5
-                - 36
+                128
+                * k**7
+                * sig_g**7
+                * (-35 * np.cos(phi) ** 4 + 30 * np.cos(phi) ** 2 - 3)
+                + 832
+                * k**5
+                * sig_g**5
+                * (-35 * np.cos(phi) ** 4 + 30 * np.cos(phi) ** 2 - 3)
+                + 36
                 * np.sqrt(np.pi)
                 * k**4
                 * sig_g**4
+                * (35 * np.cos(phi) ** 4 - 30 * np.cos(phi) ** 2 + 3)
                 * np.exp(k**2 * sig_g**2)
                 * np.erf(k * sig_g)
-                + 3100 * k**3 * sig_g**3
-                + 900
+                + 3100
+                * k**3
+                * sig_g**3
+                * (-35 * np.cos(phi) ** 4 + 30 * np.cos(phi) ** 2 - 3)
+                - 900
                 * np.sqrt(np.pi)
                 * k**2
                 * sig_g**2
+                * (35 * np.cos(phi) ** 4 - 30 * np.cos(phi) ** 2 + 3)
                 * np.exp(k**2 * sig_g**2)
                 * np.erf(k * sig_g)
-                + 7350 * k * sig_g
-                - 3675
+                - 7350 * k * sig_g * (35 * np.cos(phi) ** 4 - 30 * np.cos(phi) ** 2 + 3)
+                + 3675
                 * np.sqrt(np.pi)
+                * (35 * np.cos(phi) ** 4 - 30 * np.cos(phi) ** 2 + 3)
                 * np.exp(k**2 * sig_g**2)
                 * np.erf(k * sig_g)
             )
@@ -250,98 +275,56 @@ def K_gg_2_4(theta, phi, k, sig_g):
     return func
 
 
-def K_gv_0_0(theta, phi, k, sig_g):
+def K_gv_0_0(theta, phi, sig_g):
     def func(k):
         return 0
 
     return func
 
 
-def K_gv_0_1(theta, phi, k, sig_g):
+def K_gv_0_1(theta, phi, sig_g):
     def func(k):
-        return (
-            -3
-            / 2
-            * (
-                2 * k * sig_g
-                - np.sqrt(2)
-                * np.sqrt(np.pi)
-                * np.exp((1 / 2) * k**2 * sig_g**2)
-                * np.erf((1 / 2) * np.sqrt(2) * k * sig_g)
-            )
-            * np.exp(-1 / 2 * k**2 * sig_g**2)
-            * np.cos(phi)
-            / (k**4 * sig_g**3)
+        return -3 * np.exp(-1 / 2 * k**2 * sig_g**2) * np.cos(phi) / (
+            k**3 * sig_g**2
+        ) + (3 / 2) * np.sqrt(2) * np.sqrt(np.pi) * np.cos(phi) * np.erf(
+            (1 / 2) * np.sqrt(2) * k * sig_g
+        ) / (
+            k**4 * sig_g**3
         )
 
     return func
 
 
-def K_gv_0_2(theta, phi, k, sig_g):
+def K_gv_0_2(theta, phi, sig_g):
     def func(k):
         return 0
 
     return func
 
 
-def K_gv_0_3(theta, phi, k, sig_g):
+def K_gv_0_3(theta, phi, sig_g):
     def func(k):
         return (
             -7
-            / 32
-            * (3 * np.cos(phi) + 5 * np.cos(3 * phi))
+            / 8
+            * (5 * np.cos(phi) ** 2 - 3)
             * (
-                4 * k**3 * sig_g**3
+                4 * k**3 * sig_g**3 * np.exp((1 / 2) * k**2 * sig_g**2)
                 + 3
                 * np.sqrt(2)
                 * np.sqrt(np.pi)
                 * k**2
                 * sig_g**2
-                * np.exp((1 / 2) * k**2 * sig_g**2)
+                * np.exp(k**2 * sig_g**2)
                 * np.erf((1 / 2) * np.sqrt(2) * k * sig_g)
-                + 30 * k * sig_g
+                + 30 * k * sig_g * np.exp((1 / 2) * k**2 * sig_g**2)
                 - 15
                 * np.sqrt(2)
                 * np.sqrt(np.pi)
-                * np.exp((1 / 2) * k**2 * sig_g**2)
+                * np.exp(k**2 * sig_g**2)
                 * np.erf((1 / 2) * np.sqrt(2) * k * sig_g)
             )
-            * np.exp(-1 / 2 * k**2 * sig_g**2)
-            / (k**6 * sig_g**5)
-        )
-
-    return func
-
-
-def K_gv_0_4(theta, phi, k, sig_g):
-    def func(k):
-        return 0
-
-    return func
-
-
-def K_gv_1_0(theta, phi, k, sig_g):
-    def func(k):
-        return 0
-
-    return func
-
-
-def K_gv_1_1(theta, phi, k, sig_g):
-    def func(k):
-        return (
-            -3
-            / 2
-            * (
-                2 * k**3 * sig_g**3
-                + 6 * k * sig_g
-                - 3
-                * np.sqrt(2)
-                * np.sqrt(np.pi)
-                * np.exp((1 / 2) * k**2 * sig_g**2)
-                * np.erf((1 / 2) * np.sqrt(2) * k * sig_g)
-            )
-            * np.exp(-1 / 2 * k**2 * sig_g**2)
+            * np.exp(-(k**2) * sig_g**2)
             * np.cos(phi)
             / (k**6 * sig_g**5)
         )
@@ -349,79 +332,116 @@ def K_gv_1_1(theta, phi, k, sig_g):
     return func
 
 
-def K_gv_1_2(theta, phi, k, sig_g):
+def K_gv_0_4(theta, phi, sig_g):
     def func(k):
         return 0
 
     return func
 
 
-def K_gv_1_3(theta, phi, k, sig_g):
+def K_gv_1_0(theta, phi, sig_g):
+    def func(k):
+        return 0
+
+    return func
+
+
+def K_gv_1_1(theta, phi, sig_g):
+    def func(k):
+        return (
+            -3
+            * np.exp(-1 / 2 * k**2 * sig_g**2)
+            * np.cos(phi)
+            / (k**3 * sig_g**2)
+            - 9
+            * np.exp(-1 / 2 * k**2 * sig_g**2)
+            * np.cos(phi)
+            / (k**5 * sig_g**4)
+            + (9 / 2)
+            * np.sqrt(2)
+            * np.sqrt(np.pi)
+            * np.cos(phi)
+            * np.erf((1 / 2) * np.sqrt(2) * k * sig_g)
+            / (k**6 * sig_g**5)
+        )
+
+    return func
+
+
+def K_gv_1_2(theta, phi, sig_g):
+    def func(k):
+        return 0
+
+    return func
+
+
+def K_gv_1_3(theta, phi, sig_g):
     def func(k):
         return (
             -7
-            / 32
-            * (3 * np.cos(phi) + 5 * np.cos(3 * phi))
+            / 8
+            * (5 * np.cos(phi) ** 2 - 3)
             * (
-                4 * k**5 * sig_g**5
-                + 32 * k**3 * sig_g**3
+                4 * k**5 * sig_g**5 * np.exp(k**2 * sig_g**2)
+                + 32 * k**3 * sig_g**3 * np.exp(k**2 * sig_g**2)
                 + 9
                 * np.sqrt(2)
                 * np.sqrt(np.pi)
                 * k**2
                 * sig_g**2
-                * np.exp((1 / 2) * k**2 * sig_g**2)
+                * np.exp((3 / 2) * k**2 * sig_g**2)
                 * np.erf((1 / 2) * np.sqrt(2) * k * sig_g)
-                + 150 * k * sig_g
+                + 150 * k * sig_g * np.exp(k**2 * sig_g**2)
                 - 75
                 * np.sqrt(2)
                 * np.sqrt(np.pi)
-                * np.exp((1 / 2) * k**2 * sig_g**2)
+                * np.exp((3 / 2) * k**2 * sig_g**2)
                 * np.erf((1 / 2) * np.sqrt(2) * k * sig_g)
             )
-            * np.exp(-1 / 2 * k**2 * sig_g**2)
+            * np.exp(-3 / 2 * k**2 * sig_g**2)
+            * np.cos(phi)
             / (k**8 * sig_g**7)
         )
 
     return func
 
 
-def K_gv_1_4(theta, phi, k, sig_g):
+def K_gv_1_4(theta, phi, sig_g):
     def func(k):
         return 0
 
     return func
 
 
-def K_vv_0_0(theta, phi, k, sig_g):
+def K_vv_0_0(theta, phi, sig_g):
     def func(k):
         return (1 / 3) / k**2
 
     return func
 
 
-def K_vv_0_1(theta, phi, k, sig_g):
+def K_vv_0_1(theta, phi, sig_g):
     def func(k):
         return 0
 
     return func
 
 
-def K_vv_0_2(theta, phi, k, sig_g):
+def K_vv_0_2(theta, phi, sig_g):
     def func(k):
-        return (1 / 6) * (3 * np.cos(2 * phi) + 1) / k**2
+        return (np.cos(phi) ** 2 - 1 / 3) / k**2
 
     return func
 
 
-def K_vv_0_3(theta, phi, k, sig_g):
+def K_vv_0_3(theta, phi, sig_g):
     def func(k):
         return 0
 
     return func
 
 
-def K_vv_0_4(theta, phi, k, sig_g):
+def K_vv_0_4(theta, phi, sig_g):
     def func(k):
         return 0
 
--- flip/covariance_model/carreres23/flip_terms.py
+++ flip/covariance_model/carreres23/flip_terms.py
@@ -1,14 +1,14 @@
 import numpy as np
 
 
-def K_vv_0_0(theta, phi, k):
+def K_vv_0_0(theta, phi):
     def func(k):
         return (1 / 3) * np.cos(theta) / k**2
 
     return func
 
 
-def K_vv_0_2(theta, phi, k):
+def K_vv_0_2(theta, phi):
     def func(k):
         return (1 / 6) * (3 * np.cos(2 * phi) + np.cos(theta)) / k**2
 

662bf33b03993ed42d047137d0f3f5d84bd4c9c9
bcarreres <carreres@cppm.in2p3.fr>
2023-09-01 15:55:34 +0200

    add __init__.py for submodules

--- flip/__init__.py
+++ flip/__init__.py
@@ -1,4 +1,4 @@
 """Init file of the flip package."""
 from . import covariance 
 
-__version__ = "1.0"
+__version__ = "1.0.0"
--- flip/covariance/__init__.py
+++ flip/covariance/__init__.py
@@ -1,1 +1,2 @@
 """Init file of the flip.covariance package."""
+from . import carreres23, adamsblake20, lai22, ravouxcarreres

d545e3e2128e558726c6f1156d2a800230a8c58c
corentinravoux <corentin.ravoux.research@gmail.com>
2023-09-15 10:42:35 +0200

    construction of a proper likelihood class

--- flip/likelihood.py
+++ flip/likelihood.py
@@ -5,49 +5,85 @@ from flip.utils import create_log
 log = create_log()
 
 
-def get_coefficients_adamsblake20(parameter_values_dict):
+def get_coefficients_adamsblake20(model_type, parameter_values_dict):
     coefficients_dict = {}
-    coefficients_dict["gg"] = [
-        parameter_values_dict["bs8"] ** 2,
-        parameter_values_dict["bs8"] * parameter_values_dict["fs8"],
-        parameter_values_dict["fs8"] ** 2,
-    ]
-    coefficients_dict["gv"] = [
-        parameter_values_dict["bs8"] * parameter_values_dict["fs8"],
-        parameter_values_dict["fs8"] ** 2,
-    ]
-    coefficients_dict["vv"] = [parameter_values_dict["fs8"] ** 2]
+    if model_type in ["density", "full", "density_velocity"]:
+        coefficients_dict["gg"] = [
+            parameter_values_dict["bs8"] ** 2,
+            parameter_values_dict["bs8"] * parameter_values_dict["fs8"],
+            parameter_values_dict["fs8"] ** 2,
+        ]
+    if model_type in ["full"]:
+        coefficients_dict["gv"] = [
+            parameter_values_dict["bs8"] * parameter_values_dict["fs8"],
+            parameter_values_dict["fs8"] ** 2,
+        ]
+    if model_type in ["velocity", "full", "density_velocity"]:
+        coefficients_dict["vv"] = [parameter_values_dict["fs8"] ** 2]
     return coefficients_dict
 
 
-def get_diagonal_coefficients_adamsblake20(parameter_values_dict):
+def get_diagonal_coefficients_adamsblake20(model_type, parameter_values_dict):
     coefficients_dict = {}
-    coefficients_dict["vv"] = parameter_values_dict["sigv"] ** 2
-    coefficients_dict["gg"] = 1.0
+    if model_type in ["density", "full", "density_velocity"]:
+        coefficients_dict["gg"] = 1.0
+    if model_type in ["velocity", "full", "density_velocity"]:
+        coefficients_dict["vv"] = parameter_values_dict["sigv"] ** 2
     return coefficients_dict
 
 
 class BaseLikelihood(object):
     def __init__(
         self,
-        likelihood=None,
         covariance=None,
+        parameter_names=None,
+        vector=None,
+        vector_err=None,
     ):
-        self.likelihood = likelihood
         self.covariance = covariance
+        self.parameter_names = parameter_names
+        self.vector = vector
+        self.vector_err = vector_err
+
+    @classmethod
+    def init_from_covariance(
+        cls,
+        covariance,
+        parameter_names,
+        density=None,
+        density_err=None,
+        velocity=None,
+        velocity_err=None,
+    ):
+        if covariance.full_matrix is False:
+            covariance.compute_full_matrix()
+
+        likelihood = cls(covariance=covariance, parameter_names=parameter_names)
+
+        likelihood.vector = likelihood.compute_vector(
+            density=density,
+            velocity=velocity,
+        )
+        likelihood.vector_err = likelihood.compute_vector_err(
+            density_err=density_err,
+            velocity_err=velocity_err,
+        )
+        return likelihood
 
     def compute_covariance_sum(
         self,
         parameter_values_dict,
-        density_err=None,
-        velocity_err=None,
     ):
         coefficients_dict = eval(f"get_coefficients_{self.covariance.model_name}")(
-            parameter_values_dict
+            self.covariance.model_type,
+            parameter_values_dict,
         )
         coefficients_dict_diagonal = eval(
             f"get_diagonal_coefficients_{self.covariance.model_name}"
-        )(parameter_values_dict)
+        )(
+            self.covariance.model_type,
+            parameter_values_dict,
+        )
 
         if self.covariance.model_type == "density":
             covariance_sum = np.sum(
@@ -58,7 +94,7 @@ class BaseLikelihood(object):
                 axis=0,
             )
             covariance_sum += np.diag(
-                coefficients_dict_diagonal["gg"] * density_err**2
+                coefficients_dict_diagonal["gg"] * self.vector_err**2
             )
 
         elif self.covariance.model_type == "velocity":
@@ -70,17 +106,19 @@ class BaseLikelihood(object):
                 axis=0,
             )
             covariance_sum += np.diag(
-                coefficients_dict_diagonal["vv"] * velocity_err**2
+                coefficients_dict_diagonal["vv"] * self.vector_err**2
             )
 
         elif self.covariance.model_type in ["density_velocity", "full"]:
+            number_densities = self.covariance.number_densities
+            number_velocities = self.covariance.number_velocities
+            density_err = self.vector_err[:number_densities]
+            velocity_err = self.vector_err[
+                number_densities : number_densities + number_velocities
+            ]
+
             if self.covariance.model_type == "density_velocity":
-                covariance_sum_gv = np.zeros(
-                    (
-                        self.covariance.covariance_dict["gg"][0].shape[0],
-                        self.covariance.covariance_dict["vv"][0].shape[0],
-                    )
-                )
+                covariance_sum_gv = np.zeros((number_densities, number_velocities))
             elif self.covariance.model_type == "full":
                 covariance_sum_gv = np.sum(
                     [
@@ -96,7 +134,6 @@ class BaseLikelihood(object):
                 ],
                 axis=0,
             )
-
             covariance_sum_gg += np.diag(
                 coefficients_dict_diagonal["gg"] * density_err**2
             )
@@ -153,118 +190,25 @@ class BaseLikelihood(object):
 class MultivariateGaussianLikelihood(BaseLikelihood):
     def __init__(
         self,
-        likelihood=None,
         covariance=None,
+        parameter_names=None,
+        vector=None,
+        vector_err=None,
     ):
         super(MultivariateGaussianLikelihood, self).__init__(
-            likelihood=likelihood,
             covariance=covariance,
+            parameter_names=parameter_names,
+            vector=vector,
+            vector_err=vector_err,
         )
 
-    @classmethod
-    def init_from_covariance(
-        cls,
-        covariance,
-        density=None,
-        density_err=None,
-        velocity=None,
-        velocity_err=None,
-    ):
-        multivariate_gaussian_likelihood = cls(covariance=covariance)
-        vector = multivariate_gaussian_likelihood.compute_vector(
-            density=density,
-            velocity=velocity,
+    def __call__(self, parameter_values):
+        parameter_values_dict = dict(zip(self.parameter_names, parameter_values))
+
+        covariance_sum = self.compute_covariance_sum(
+            parameter_values_dict,
         )
-
-        def log_likelihood(parameter_values_dict):
-            covariance_sum = multivariate_gaussian_likelihood.compute_covariance_sum(
-                parameter_values_dict,
-                density_err=density_err,
-                velocity_err=velocity_err,
-            )
-            cholesky = cho_factor(covariance_sum)
-            logdet = 2 * np.sum(np.log(np.diag(cholesky[0])))
-            chi2 = np.dot(vector, cho_solve(cholesky, vector))
-            return -0.5 * (vector * np.log(2 * np.pi) + logdet + chi2)
-
-        multivariate_gaussian_likelihood.likelihood = log_likelihood
-        return multivariate_gaussian_likelihood
-
-
-# CR - cleaner with factory etc:
-
-# class VelocityLikelihood(MultivariateGaussianLikelihood):
-#     def __init__(self, likelihood=None, model_name=None, covariance=None, vector=None):
-#         self.vector = vector
-#         self.covariance = covariance
-#         self.model_name = model_name
-
-
-# class DensityLikelihood(MultivariateGaussianLikelihood):
-#     def __init__(self, function=None, rsd=None):
-#         self.function = function
-#         self.rsd = rsd
-
-#     def get_log_like_density(
-#         self, fitpar, density, density_err, cov_gg_b2, cov_gg_bf, cov_gg_f2, m_index_gg
-#     ):
-#         fs8 = fitpar[0]
-#         bs8 = fitpar[1]
-#         sig_g = fitpar[2]
-
-#         # Build cov matrix
-#         cov_gg_b2_all = np.sum(
-#             np.array([cov_gg_b2[i] * sig_g**m for i, m in enumerate(m_index_gg)]),
-#             axis=0,
-#         )
-#         cov_gg_bf_all = np.sum(
-#             np.array([cov_gg_bf[i] * sig_g**m for i, m in enumerate(m_index_gg)]),
-#             axis=0,
-#         )
-#         cov_gg_f2_all = np.sum(
-#             np.array([cov_gg_f2[i] * sig_g**m for i, m in enumerate(m_index_gg)]),
-#             axis=0,
-#         )
-#         cov_matrix = (
-#             bs8**2 * cov_gg_b2_all
-#             + bs8 * fs8 * cov_gg_bf_all
-#             + fs8**2 * cov_gg_f2_all
-#         )
-
-#         # Add shot noise
-#         diag_add = density_err**2
-#         cov_matrix += np.diag(diag_add)
-
-#         log_like = self.log_likelihood(density, cov_matrix)
-#         return log_like
-
-#     def get_log_like_density_no_rsd(
-#         self, fitpar, density, density_err, cov_gg_b2, m_index_gg
-#     ):
-#         bs8 = fitpar[0]
-#         sig_g = fitpar[1]
-
-#         # Build cov matrix
-#         cov_gg_b2_all = np.sum(
-#             np.array([cov_gg_b2[i] * sig_g**m for i, m in enumerate(m_index_gg)]),
-#             axis=0,
-#         )
-#         cov_matrix = bs8**2 * cov_gg_b2_all
-
-#         # Add shot noise
-#         diag_add = density_err**2
-#         cov_matrix += np.diag(diag_add)
-
-#         log_like = self.log_likelihood(density, cov_matrix)
-#         return log_like
-
-#     @classmethod
-#     def init_from_matrix(cls):
-#         return cls.init_from_property_files()
-
-
-# class JointLikelihood(MultivariateGaussianLikelihood):
-#     def __init__(self, likelihood=None, model_name=None, covariance=None, vector=None):
-#         self.vector = vector
-#         self.covariance = covariance
-#         self.model_name = model_name
+        cholesky = cho_factor(covariance_sum)
+        logdet = 2 * np.sum(np.log(np.diag(cholesky[0])))
+        chi2 = np.dot(self.vector, cho_solve(cholesky, self.vector))
+        return 0.5 * (self.vector.size * np.log(2 * np.pi) + logdet + chi2)

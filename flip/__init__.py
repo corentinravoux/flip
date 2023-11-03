150490c41af9944a205bda9060cd20978035000e
bcarreres <carreres@cppm.in2p3.fr>
2023-11-03 15:41:38 -0400

    API major update

--- flip/__init__.py
+++ flip/__init__.py
@@ -1,4 +1,5 @@
 """Init file of the flip package."""
+import os
 from . import covariance
 from . import fitter
 from . import gridding
@@ -7,3 +8,4 @@ from . import model_evaluation
 from . import utils
 
 __version__ = "1.0.0"
+__flip_dir_path__ =  os.path.dirname(__file__)

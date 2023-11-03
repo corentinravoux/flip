24cd866c985e67ab1278855122eab763a84f6ebf
bcarreres <carreres@cppm.in2p3.fr>
2023-11-03 14:28:33 -0400

    Test docs

---
+++ docs/api.rst
@@ -0,0 +1,17 @@
+API
+====
+
+.. currentmodule:: flip
+
+.. autosummary::
+   :toctree: generated
+   :recursive:
+
+   flip.covariance
+   flip.likelihood
+   flip.fitter
+   flip.gridding
+   flip.model_evaluation
+   flip.power_spectra
+   flip.utils
+
--- flip/__init__.py
+++ flip/__init__.py
@@ -1,4 +1,9 @@
 """Init file of the flip package."""
-from . import covariance 
+from . import covariance
+from . import fitter
+from . import gridding
+from . import likelihood
+from . import model_evaluation
+from . import utils
 
 __version__ = "1.0.0"

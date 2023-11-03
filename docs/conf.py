4693f565897576978b049acb76bee0f110e2ac89
Bastien Carreres <bastien.carreres@duke.edu>
2023-11-03 15:53:17 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ sys.path.insert(0, os.path.abspath('..'))
 import flip 
 
 project = 'flip'
-copyright = '2023, C. Ravoux'
+copyright = '2023, C. Ravoux, B. Carreres'
 author = 'C. Ravoux, B. Carreres'
 
 # The full version, including alpha/beta/rc tags

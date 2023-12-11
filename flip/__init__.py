aea33e02b34c8793de46232e4099b783af3e5b2b
corentinravoux <corentin.ravoux.research@gmail.com>
2023-12-11 13:17:51 +0100

    fix

--- flip/__init__.py
+++ flip/__init__.py
@@ -1,13 +1,7 @@
 """Init file of the flip package."""
 import os
-from . import covariance
-from . import fitter
-from . import gridding
-from . import likelihood
-from . import model_evaluation
-from . import utils
-from . import power_spectra
 
+from . import covariance, fitter, gridding, likelihood, power_spectra, utils
 
 __version__ = "1.0.0"
-__flip_dir_path__ =  os.path.dirname(__file__)
+__flip_dir_path__ = os.path.dirname(__file__)

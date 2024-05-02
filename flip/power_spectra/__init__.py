de36c80980ac8cae362e650c730dca674fc6a09f
corentinravoux <corentin.ravoux.research@gmail.com>
2024-05-02 16:54:14 +0200

    renaming of power_spectra_generator

--- flip/power_spectra/__init__.py
+++ flip/power_spectra/__init__.py
@@ -1,3 +1,4 @@
 """Init file of the flip.power_spectra package."""
+
 from . import class_engine, models
-from .power_spectra_generator import compute_power_spectra
+from .generator import compute_power_spectra

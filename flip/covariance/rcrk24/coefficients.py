f0fd3b044d04c00ad6852f2c81f9fbbad0f3f2d4
corentinravoux <corentin.ravoux.research@gmail.com>
2024-07-16 18:03:12 +0200

    implementation of new model Rosseli, Carreres, Ravoux, Kim

---
+++ flip/covariance/rcrk24/coefficients.py
@@ -0,0 +1,31 @@
+import numpy as np
+from astropy.cosmology import FlatLambdaCDM
+
+
+def get_coefficients(
+    model_type,
+    parameter_values_dict,
+    variant=None,
+    redshift_dict=None,
+    power_spectrum_amplitude_function=None,
+):
+    coefficients_dict = {}
+    redshift_velocities = redshift_dict["v"]
+    cosmo = FlatLambdaCDM(H0=100, Om0=parameter_values_dict["Om0"])
+
+    coefficient_vector = (
+        cosmo.Om(redshift_velocities).value ** parameter_values_dict["gamma"]
+        * cosmo.H(redshift_velocities).value
+        / cosmo.H0
+        * power_spectrum_amplitude_function(redshift_velocities)
+        / (1 + redshift_velocities)
+    )
+
+    coefficients_dict["vv"] = [np.outer(coefficient_vector)]
+    return coefficients_dict
+
+
+def get_diagonal_coefficients(model_type, parameter_values_dict):
+    coefficients_dict = {}
+    coefficients_dict["vv"] = parameter_values_dict["sigv"] ** 2
+    return coefficients_dict

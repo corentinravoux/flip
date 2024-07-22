95e232c6b11ae8c384128405b0f9682eb6aace0c
corentinravoux <corentin.ravoux.research@gmail.com>
2024-07-22 17:55:26 +0200

    fix of the outer term

--- flip/covariance/rcrk24/fisher_terms.py
+++ flip/covariance/rcrk24/fisher_terms.py
@@ -13,37 +13,49 @@ def get_partial_derivative_coefficients(
     cosmo = FlatLambdaCDM(H0=100, Om0=parameter_values_dict["Om0"])
     redshift_velocities = redshift_dict["v"]
 
+    Omega_m_partial_derivative_coefficients = (
+        2
+        * cosmo.Om(redshift_velocities).value ** (2 * parameter_values_dict["gamma"])
+        * parameter_values_dict["gamma"]
+        * power_spectrum_amplitude_function(redshift_velocities) ** 2
+        / cosmo.Om(redshift_velocities).value
+    )
+
+    gamma_partial_derivative_coefficients = (
+        2
+        * cosmo.Om(redshift_velocities).value ** (2 * parameter_values_dict["gamma"])
+        * power_spectrum_amplitude_function(redshift_velocities) ** 2
+        * np.log(cosmo.Om(redshift_velocities).value)
+    )
+
+    s8_partial_derivative_coefficients = (
+        2
+        * cosmo.Om(redshift_velocities).value ** (2 * parameter_values_dict["gamma"])
+        * power_spectrum_amplitude_function(redshift_velocities)
+    )
+
     partial_coefficients_dict = {
         "Omegam": {
             "vv": [
                 np.outer(
-                    2
-                    * cosmo.Om(redshift_velocities).value
-                    ** (2 * parameter_values_dict["gamma"])
-                    * parameter_values_dict["gamma"]
-                    * power_spectrum_amplitude_function(redshift_velocities) ** 2
-                    / cosmo.Om(redshift_velocities).value
+                    Omega_m_partial_derivative_coefficients,
+                    Omega_m_partial_derivative_coefficients,
                 ),
             ],
         },
         "gamma": {
             "vv": [
                 np.outer(
-                    2
-                    * cosmo.Om(redshift_velocities).value
-                    ** (2 * parameter_values_dict["gamma"])
-                    * power_spectrum_amplitude_function(redshift_velocities) ** 2
-                    * np.log(cosmo.Om(redshift_velocities).value)
+                    gamma_partial_derivative_coefficients,
+                    gamma_partial_derivative_coefficients,
                 ),
             ],
         },
         "s8": {
             "vv": [
                 np.outer(
-                    2
-                    * cosmo.Om(redshift_velocities).value
-                    ** (2 * parameter_values_dict["gamma"])
-                    * power_spectrum_amplitude_function(redshift_velocities)
+                    s8_partial_derivative_coefficients,
+                    s8_partial_derivative_coefficients,
                 ),
             ],
         },

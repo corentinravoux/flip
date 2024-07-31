6266023e1b07de2d96e92c0a3f50e15e3db761e7
corentinravoux <corentin.ravoux.research@gmail.com>
2024-07-31 11:32:13 +0200

    fixing power spectrum script

--- scripts/flip_compute_power_spectra.py
+++ scripts/flip_compute_power_spectra.py
@@ -15,11 +15,16 @@ minimal_wavenumber = 0.0005
 maximal_wavenumber = 1.000
 number_points = 1000
 logspace = True
-redshift = 0.11
-normalize_power_spectrum = True  # Normalize by sigma_8
-power_spectrum_non_linear_model = (
-    "halofit"  # If you want to add non linearity, what model to use
-)
+redshift = 0.0
+
+# Can be changed by "growth_rate" (fsigma_8^fid normalization) or "no_normalization"
+# Here, it normalize by sigma_8^fid
+normalization_power_spectrum = "growth_amplitude"
+
+# If you want to add non linearity, what model to use
+power_spectrum_non_linear_model = "halofit"
+
+# Model used to compute matter and velocity divergence power spectra
 power_spectrum_model = "linearbel"
 
 save_path = "./"  # If not None, will save all calculated power spectra in this folder
@@ -37,7 +42,7 @@ save_path = "./"  # If not None, will save all calculated power spectra in this 
     maximal_wavenumber,
     number_points,
     logspace=logspace,
-    normalize_power_spectrum=normalize_power_spectrum,
+    normalization_power_spectrum=normalization_power_spectrum,
     power_spectrum_non_linear_model=power_spectrum_non_linear_model,
     power_spectrum_model=power_spectrum_model,
     save_path=save_path,

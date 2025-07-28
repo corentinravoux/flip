b0af5f51b69d96ff4a54d03b238259b80ebf9085
Bastien Carreres <bastien.carreres@duke.edu>
2025-07-28 16:22:14 -0400

    add possibility to load example data

--- flip/data/__init__.py
+++ flip/data/__init__.py
@@ -1,3 +1,12 @@
 """Init file of the flip.data package."""
 
-from .data import *
+def load_velocity_data():
+    import pandas as pd
+    from flip import __flip_dir_path__
+    return pd.read_parquet(__flip_dir_path__ + '/data/velocity_data.parquet')
+
+
+def load_density_data():
+    import pandas as pd
+    from flip import __flip_dir_path__
+    return pd.read_parquet(__flip_dir_path__ + '/data/density_data.parquet')

abf11f628554febbf6130a4df64429c628b66e27
corentinravoux <corentin.ravoux.research@gmail.com>
2025-12-02 17:44:15 +0100

    small improvement on packaging

--- flip/__init__.py
+++ flip/__init__.py
@@ -24,5 +24,6 @@ try:
 except:
     log.add("Jax is not available, loading numpy and scipy instead")
 
-__version__ = "1.0.0"
+from ._version import __version__
+
 __flip_dir_path__ = os.path.dirname(__file__)

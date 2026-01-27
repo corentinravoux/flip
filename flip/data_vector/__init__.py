dc9a16348b33b2c8c10b94181ff3312e0ad82cda
corentinravoux <corentin.ravoux.research@gmail.com>
2026-01-27 17:25:04 +0100

    moving gridding on the good package

--- flip/data_vector/__init__.py
+++ flip/data_vector/__init__.py
@@ -1,4 +1,4 @@
 """Init file of the flip.data_vector package."""
 
-from . import cosmo_utils, galaxypv_vectors, snia_vectors
+from . import cosmo_utils, galaxypv_vectors, gridding, snia_vectors, vector_utils
 from .basic import *

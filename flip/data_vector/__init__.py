af59607c4c0957d16d58770b1679b1e3e4e21232
corentinravoux <corentin.ravoux.research@gmail.com>
2026-03-12 17:54:31 +0100

    deleteing replaced gridding module

--- flip/data_vector/__init__.py
+++ flip/data_vector/__init__.py
@@ -1,4 +1,4 @@
 """Init file of the flip.data_vector package."""
 
-from . import cosmo_utils, galaxypv_vectors, gridding, snia_vectors, vector_utils
+from . import cosmo_utils, galaxypv_vectors, snia_vectors, vector_utils, gw_vectors, mesh
 from .basic import *
